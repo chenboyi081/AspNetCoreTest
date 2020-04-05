76ddc5054c9b922cf6e57a49719d60863ff0eeec
chenboyi081 <chenboyi081@163.com>
2020-04-06 07:09:15 +0800

    添加jieke11

---
+++ jike_11_Configuration_FileDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+namespace jike_11_Configuration_FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            var configurationRoot = builder.Build();
+
+            Console.WriteLine($"Key1:{configurationRoot["Key1"]}");
+            Console.WriteLine($"Key5:{configurationRoot["Key5"]}");
+            Console.WriteLine($"Key6:{configurationRoot["Key6"]}");
+        }
+    }
+}
