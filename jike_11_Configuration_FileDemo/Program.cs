679e874aed85fa6d1a21dfba957a0bc8af65c3ea
chenboyi081 <chenboyi081@163.com>
2020-04-23 04:12:51 +0800

    极客第12课内容代码（归到jike_11这个文件中）

--- jike_11_Configuration_FileDemo/Program.cs
+++ jike_11_Configuration_FileDemo/Program.cs
@@ -5,15 +5,28 @@ namespace jike_11_Configuration_FileDemo
 {
     class Program
     {
+        //另可参考：【ASP.NET Core快速入门】（六）配置的热更新、配置的框架设计：https://www.cnblogs.com/wyt007/p/8053968.html
         static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder();
-            builder.AddJsonFile("appsettings.json");
+            builder.AddJsonFile("appsettings.json",optional:true,reloadOnChange:true);    
+            //builder.AddIniFile("appsettings.ini");  //后添加的文件优先级更高
             var configurationRoot = builder.Build();
 
-            Console.WriteLine($"Key1:{configurationRoot["Key1"]}");
-            Console.WriteLine($"Key5:{configurationRoot["Key5"]}");
-            Console.WriteLine($"Key6:{configurationRoot["Key6"]}");
+            //IChangeToken仅在文件发生变更时触发
+            IChangeToken token = configurationRoot.GetReloadToken();
+            //IChangeToken只能使用一次,所以  使用Onchange方法绑定configurationRoot.GetReloadToken()，每次获取新的token
+            ChangeToken.OnChange(() => configurationRoot.GetReloadToken(), () =>
+            {
+                Console.WriteLine($"Key1:{configurationRoot["Key1"]}");
+                Console.WriteLine($"Key2:{configurationRoot["Key2"]}");
+                Console.WriteLine($"Key3:{configurationRoot["Key3"]}");
+            });
+
+            //Console.WriteLine($"Key1:{configurationRoot["Key1"]}");
+            //Console.WriteLine($"Key2:{configurationRoot["Key2"]}");
+            //Console.WriteLine($"Key3:{configurationRoot["Key3"]}");
+            Console.ReadKey();
         }
     }
 }
